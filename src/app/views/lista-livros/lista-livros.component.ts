cf35cd0c02b8a20bef3eaa3d4459cc1a56faabb6
Marcos Vogado <marcoshcvogado@gmail.com>
2025-10-13 17:31:26 -0300

    Update Google Books API endpoint and refactor subscription
    Changed the API endpoint in LivroService from 'volumes' to 'volume'. Refactored the subscription in ListaLivrosComponent to use the object form for better readability and future extensibility.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -15,9 +15,10 @@ export class ListaLivrosComponent {
   constructor(private service: LivroService) { }
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI)
-  )
+    this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(retornoAPI)
+    })
+    
   }
 
 }
