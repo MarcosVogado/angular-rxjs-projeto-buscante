cedee177c35415cd8729dc12b68122283f92977e
Marcos Vogado <marcoshcvogado@gmail.com>
2025-10-16 15:58:04 -0300

    Implement autoria pipe and update livro component
    Added logic to the AutoriaPipe to return the first author from the array. Updated livro.component.html to use the autoria pipe and limit the displayed author string to 20 characters.

--- src/app/pipes/autoria.pipe.ts
+++ src/app/pipes/autoria.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AutoriaPipe implements PipeTransform {
 
-  transform(autoria: string[]): unknown {
-    return null;
+  transform(autoria: string[]): string {
+    if(autoria) {
+      return autoria[0]
+    } else {
+      return '';
+    }
+    
   }
 
 }
