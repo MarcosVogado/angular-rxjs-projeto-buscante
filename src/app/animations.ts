c4970f891a4bf0e87d5f8e0e3aded507b2405fc9
Marcos Vogado <marcoshcvogado@gmail.com>
2025-11-05 16:28:31 -0300

    Update animations.ts

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -2,36 +2,21 @@ import { animate, keyframes, query, stagger, style, transition, trigger } from "
 
 export const listStateTrigger = trigger('listState', [
   transition('* => *', [
+    // entrada
     query(':enter', [
-      style({
-        opacity: 0,
-        transform: 'translateY(-100%)'
-      }),
+      style({ opacity: 0, transform: 'translateY(-100%)' }),
       stagger(200, [
         animate('500ms ease-in', keyframes([
-          style({
-            opacity: 1,
-            transform: 'translateY(-10%)',
-            offset: 0.4
-          }),
-          style({
-            opacity: 1,
-            transform: 'translateY(0)',
-            offset: 1
-          }),
+          style({ opacity: 1, transform: 'translateY(-10%)', offset: 0.4 }),
+          style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
         ]))
-      ]),
+      ])
+    ], { optional: true }),
+    // saída
     query(':leave', [
-        stagger(200, [
-        animate('500ms ease-in', keyframes([
-          style({
-            opacity: 0,
-            transform: 'translateY(-100%)',
-            offset: 0.4
-          })
-        ]))
-    ])
-    ], {optional: true})
+      stagger(200, [
+        animate('400ms ease-in', style({ opacity: 0, transform: 'translateY(-100%)' }))
+      ])
+    ], { optional: true })
   ])
-])
-])
+]);
